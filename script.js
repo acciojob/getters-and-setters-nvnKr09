c253d29e4a90fa02849e3a27e6b49c13ab279240
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-11-24 23:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 //complete this code
 class Person {
 	constructor(name, age){
-		this.name = name;
-	    this.age = age;
+		this.__name = name;
+	    this._age = age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
 	set age(myAge){
-		this.age = myAge;
+		this._age = myAge;
 	}
 }
 
 class Student extends Person {
+	constructor(name, age) {
+		super(name, age); // Call the constructor of the parent class
+	}
 	study(){
 		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
+	constructor(name, age) {
+		super(name, age); // Call the constructor of the parent class
+	}
 	teach(){
 		console.log(`${this.name} is teaching`);
 	}
